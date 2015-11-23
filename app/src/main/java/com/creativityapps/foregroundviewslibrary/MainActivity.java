036322fe2fe3c34147c48eb015ce0dd665e2fb45
Yesid Lazaro <yesidlazaro@Yesids-MacBook-Pro.local>
2015-11-23 12:07:14 -0500

    initial commit

---
+++ app/src/main/java/com/creativityapps/foregroundviewslibrary/MainActivity.java
@@ -0,0 +1,23 @@
+package com.creativityapps.foregroundviewslibrary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.imageview, R.id.relativelayout, R.id.linearlayout})
+    protected void onclickView(View view) {
+
+    }
+}
